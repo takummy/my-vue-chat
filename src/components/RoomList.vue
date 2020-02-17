8fe3f952ff86e7b8af96b49fc74299c9d11229bb
Your Actual Name <Your Actual Email>
2020-02-17 18:17:07 +0900

    NEW - RoomList.vueを作成

---
+++ src/components/RoomList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="room-list">
+    <h4>チャンネル</h4>
+    <hr>
+    <b-list-group v-if="activeRoom">
+      <b-list-group-item v-for:="room in rooms"
+                         :key="room.name"
+                         :active="activeRoom.id == room.id"
+                         href="#"
+                         @click="onChange(room)">
+        # {{ room.name }}
+      </b-list-group-item>
+    </b-list-group>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'room-list',
+  computed: {
+    ...mapState([
+      'rooms',
+      'activeRoom'
+    ])
+  }
+}
+</script>
