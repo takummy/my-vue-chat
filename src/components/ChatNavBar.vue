f987059d4362401f252ce0f7eb9dc9ec3f0b7e0b
Your Actual Name <Your Actual Email>
2020-02-17 18:08:54 +0900

    NEW - ChatNavBar.vueを作成

---
+++ src/components/ChatNavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar id="chat-navbar togglable="md" type="dark variant="info">
+    <b-navbar-brand href="#">
+      びゅーちゃっと
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-text>{{ user.name }} | </b-nav-text>
+      <b-nav-item href="#" active>ログアウト</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'chatnavbar',
+  computed: {
+    ...mapState([
+      'user'
+    ])
+  }
+}
+</script>
+
+<style>
+  #chat-navbar {
+    margin-bottom: 15px;
+  }
+</style>
